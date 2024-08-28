47f8707729a5099646998555ef67b3fdc0b5e197
muumin <harada.kaito.20@shizuoka.ac.jp>
2024-08-28 22:18:25 +0900

    [add] クエリパラメータの追加

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def main(private_key, public_key):
+def main(private_key, public_key, period):
     calendar = GoogleCalendarTools(private_key=private_key, public_key=public_key)
-    events = calendar.get_events(period=60)
+    events = calendar.get_events(period=period)
     return events
 
 @app.get("/test")
