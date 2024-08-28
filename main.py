036f2534cd4e160d1cd009ff85baee80693011ca
muumin <harada.kaito.20@shizuoka.ac.jp>
2024-08-28 22:33:35 +0900

    [add] エラー処理の追加

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ app = FastAPI()
 
 @app.get("/")
 def main(private_key, public_key, period):
-    calendar = GoogleCalendarTools(private_key=private_key, public_key=public_key)
-    events = calendar.get_events(period=int(period))
-    return events
+    try:
+        calendar = GoogleCalendarTools(private_key=private_key, public_key=public_key)
+        events = calendar.get_events(period=int(period))
+        return events
+    except Exception as e:
+        return {"error": str(e)}
 
 @app.get("/test")
 def test():
