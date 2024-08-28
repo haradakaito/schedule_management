5abff3aa823626bcf8434ce401b5f6b6f1675141
muumin <harada.kaito.20@shizuoka.ac.jp>
2024-08-28 21:42:32 +0900

    [add] .gitignoreの修正

---
+++ main.py
@@ -0,0 +1,13 @@
+from _calendar import GoogleCalendarTools
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")
+def main():
+    calendar = GoogleCalendarTools()
+    events = calendar.get_events(period=60)
+    return {events}
+
+@app.get("/test")
+def test():
+    return {"Hello": "World"}
