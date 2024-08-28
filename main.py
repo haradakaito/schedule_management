502f3221ab28020ad5310ed358698eb4f25fc0c5
muumin <harada.kaito.20@shizuoka.ac.jp>
2024-08-28 22:18:01 +0900

    [rm] コメントアウト除去

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ app = FastAPI()
 
 @app.get("/")
 def main(private_key, public_key):
-    # private_key = 'c04003ead0b0adae584dde596f3496ea'
-    # public_key = '33bb8e7fee31a8263ba0be122b8adc87'
     calendar = GoogleCalendarTools(private_key=private_key, public_key=public_key)
     events = calendar.get_events(period=60)
     return events
