b967f8c9fb6c338665f3c16b2b0b19ed91d425ec
muumin <harada.kaito.20@shizuoka.ac.jp>
2024-08-28 21:55:46 +0900

    [add] クエリパラメータの修正

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ app = FastAPI()
 
 @app.get("/")
 def main(private_key, public_key):
-    private_key = 'private-c04003ead0b0adae584dde596f3496ea'
-    public_key = 'private-33bb8e7fee31a8263ba0be122b8adc87'
+    private_key = 'c04003ead0b0adae584dde596f3496ea'
+    public_key = '33bb8e7fee31a8263ba0be122b8adc87'
     calendar = GoogleCalendarTools(private_key=private_key, public_key=public_key)
     events = calendar.get_events(period=60)
     return {events}
