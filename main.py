9b5cfc9137e89347e7e80243c754aaab8688f927
muumin <harada.kaito.20@shizuoka.ac.jp>
2024-08-28 21:34:09 +0900

    [add] Koyeb用に改変

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from _calendar import GoogleCalendarTools
+from fastapi import FastAPI
+app = FastAPI()
 
+@app.get("/")
 def main():
     calendar = GoogleCalendarTools()
     events = calendar.get_events(period=60)
     return events
 
-if __name__ == '__main__':
-    main()
+@app.get("/test")
+def test():
+    return {"Hello": "World"}
